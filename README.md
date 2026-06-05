06fe138b83b3e32ba29ba1d21240b71e56efa473
Hongyi Xiao <249873180+chestnutcapybara@users.noreply.github.com>
2026-06-04 19:47:40 -0700

    Started moving the bird journal app to PySide6 from CTk

--- README.md
+++ README.md
@@ -1,9 +1,4 @@
 # Bird Journal
 ### What is Bird Journal?
 Bird Journal is a application in where you can save your favorite bird sightings.
-Bird Journal is made with the library customtkinter (CTk).
-## Current Features
-* Add New Bird
-    * Bird name
-    * Notes
-* Add Bird to Favorites
+Bird Journal is made with the popular Python bindings for Qt, PySide6.
--- main.py
+++ main.py
@@ -1,5 +1,42 @@
-from app import App
+from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QPushButton, QLabel, QLineEdit
+import sys
+
+
+### The Main WIndow class ###
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Bird Journal 🐦")
+        self.setGeometry(0,0, 600, 400)
+        
+        # Apply the Always on Top flag
+        self.setWindowFlags(self.windowFlags() | Qt.WindowType.WindowStaysOnTopHint)
+        self.show()
+        self.init_ui()
+
+    def init_ui(self):
+        container = QWidget() # create a container, which holds all the other widgets.
+        layout = QVBoxLayout() # q Vertical Box Layout
+        container.setLayout(layout)
+        self.setCentralWidget(container) # set the container as the centreral widget
+
+        ### Ui Things ###
+        add_bird_btn = QPushButton("+ Add Bird")
+        add_bird_btn.clicked.connect(self.add_bird)
+        add_bird_btn.setMinimumHeight(35)
+        add_bird_btn.setMinimumWidth(100)
+        layout.addWidget(add_bird_btn, alignment=Qt.AlignmentFlag.AlignTop | Qt.AlignmentFlag.AlignLeft)
+
+    def add_bird(self):
+        pass
+
+
+def main():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    sys.exit(app.exec())
 
 if __name__ == "__main__":
-    app = App()
-    app.mainloop()
+    main()
